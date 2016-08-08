7743fad7cc2941cf81f83c6125b49ba7f5e19d93
NgocSon2412 <sonnguyenk57@gmail.com>
2016-08-08 10:23:27 +0700

    init-model

---
+++ database/migrations/2016_08_08_023504_create_user_course_subjects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserCourseSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_course_subjects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->integer('course_subject_id')->index();
+            $table->integer('subject_id')->index();
+            $table->integer('progress');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_course_subjects');
+    }
+}
